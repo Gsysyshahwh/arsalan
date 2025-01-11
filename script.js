cdcd763897c1f42ccfbacb57c5de6881144a5630
Gsysyshahwh <azaankhaann111@gmail.com>
2025-01-11 11:17:39 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('revealButton').addEventListener('click', function () {
+    const message = document.getElementById('revealMessage');
+    message.style.display = 'block';
+});
